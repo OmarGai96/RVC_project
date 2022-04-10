00be4190f9bc061d20539631a72bd679dbcec114
Omar <gai.omar96@gmail.com>
2022-04-10 12:26:22 +0200

    Added first version of Task3 and Task4

---
+++ RVC_Oses/libraries/examples/event_sample.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2006-2021, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2022-04-06     Omar       the first version
+ */
+#ifndef EVENT_SAMPLE_H
+#define EVENT_SAMPLE_H
+
+int event_sample(void);
+
+#endif
